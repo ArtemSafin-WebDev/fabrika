0e723808956d7252a0c7b292f641692e306a01b3
Artem Safin <artemsafin.webdev@gmail.com>
2021-12-31 02:40:37 +0300

    Блок достижений

--- src/js/achivementsSlider.js
+++ src/js/achivementsSlider.js
@@ -4,6 +4,9 @@ export default function achivementsSlider() {
     const elements = Array.from(document.querySelectorAll('.js-achivements-slider'));
 
     elements.forEach(element => {
+
+        if (!window.matchMedia("(max-width: 640px)").matches) return;
+
         const container = element.querySelector('.swiper-container');
 
         new Swiper(container, {
