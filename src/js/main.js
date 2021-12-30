1bb4f0d12447911d502f16ae5bee476e9fc608b2
Artem Safin <artemsafin.webdev@gmail.com>
2021-12-30 23:34:25 +0300

    По блоку с цифрами

--- src/js/main.js
+++ src/js/main.js
@@ -43,7 +43,7 @@ document.addEventListener('DOMContentLoaded', function() {
     achivementsSlider();
     citySearch();
     fancybox();
-    agesSlider();
+    // agesSlider();
     menu();
     counter();
     showMore();
